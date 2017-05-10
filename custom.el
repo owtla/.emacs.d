91696c1fa633b5708890e621fa9ccbcd6758504a
owtla <owtla@earthlink.net>
2017-05-09 21:20:31 -0700

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-safe-themes
+   (quote
+    ("38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(fci-rule-color "#383838")
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode smart-mode-line rainbow-delimiters py-autopep8 osx-clipboard markdown-preview-mode markdown-mode magit json-mode js2-mode go-guru go-eldoc go-autocomplete go-add-tags flycheck exec-path-from-shell elpy cyberpunk-theme better-defaults)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
